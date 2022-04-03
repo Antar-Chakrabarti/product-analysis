34a3fff22458d2cf750f9d17e24a3db65fe44189
Antar-chakrabarti <chakrabartiantar@gmail.com>
2022-04-03 21:22:02 +0600

    header added

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='home-area'>
+            <div className="top-home-area">
+                <div className="text-area"></div>
+                <div className="img-area"></div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
