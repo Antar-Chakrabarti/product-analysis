90c9ef7f2e7dee561a6fd3e693f4816ff6fa8b15
Antar-chakrabarti <chakrabartiantar@gmail.com>
2022-04-04 12:28:15 +0600

    chart added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Review from './components/Review/Review';
+import DashBoard from './DashBoard/DashBoard';
+import Footer from './Footer/Footer';
 function App() {
   return (
     <div>
@@ -10,7 +12,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/reviews' element={<Review/>}></Route>
+        <Route path='dashboard' element={<DashBoard/>}></Route>
       </Routes>
+      <Footer/>
     </div>
   );
 }
---
+++ src/DashBoard/DashBoard.js
@@ -0,0 +1,68 @@
+import { Bar, BarChart, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import './DashBoard.css';
+const DashBoard = () => {
+    const data = [
+        {
+            "month": "Mar",
+            "investment": 100000,
+            "sell": 241,
+            "revenue": 10401
+        },
+        {
+            "month": "Apr",
+            "investment": 200000,
+            "sell": 423,
+            "revenue": 24500
+        },
+        {
+            "month": "May",
+            "investment": 500000,
+            "sell": 726,
+            "revenue": 67010
+        },
+        {
+            "month": "Jun",
+            "investment": 500000,
+            "sell": 529,
+            "revenue": 40405
+        },
+        {
+            "month": "Jul",
+            "investment": 600000,
+            "sell": 601,
+            "revenue": 50900
+        },
+        {
+            "month": "Aug",
+            "investment": 700000,
+            "sell": 670,
+            "revenue": 61000
+        }
+    ]
+    return (
+        <div className='dashboard-area'>
+            <div className="chart-one">
+                <LineChart width={500} height={400} data={data}>
+                    <Line type="monotone" dataKey="sell" stroke="#8884d8"></Line>
+                    <Line type="monotone" dataKey="investment" stroke="#82ca9d" />
+                    <YAxis />
+                    <XAxis />
+                    <Tooltip />
+                    <Legend />
+                </LineChart>
+            </div>
+            <div className="chart-two">
+                <BarChart width={500} height={400} data={data}>
+                    <Bar type="monotone" dataKey="sell" stroke="#8884d8"></Bar>
+                    <Bar type="monotone" dataKey="investment" stroke="#82ca9d" />
+                    <YAxis />
+                    <XAxis />
+                    <Tooltip />
+                    <Legend />
+                </BarChart>
+            </div>
+        </div>
+    );
+};
+
+export default DashBoard;
